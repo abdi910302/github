bf83af4352ddf374da216923f3d2ae7910304fb3
yuying chen <abdi910302@gmaail.com>
2023-09-13 16:43:00 +0800

    Initial Commit

---
+++ github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by 陳昱穎 on 2023/9/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
